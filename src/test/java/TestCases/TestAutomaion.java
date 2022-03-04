1b549aa397732ec5eeb8bf660e2d825c3b0d4ccf
Ramachandra <ramureddy4555@gmail.com>
2022-03-04 08:34:29 +0530

    Test automation

---
+++ src/test/java/TestCases/TestAutomaion.java
@@ -0,0 +1,11 @@
+package TestCases;
+
+import org.apache.commons.collections4.bag.SynchronizedSortedBag;
+
+public class TestAutomaion {
+	
+	public static void main(String [] args) {
+		System.out.println("Testing");
+	}
+
+}
